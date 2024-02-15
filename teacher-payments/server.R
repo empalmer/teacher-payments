2f1b38e408208ed6f34b2c5dbae17a6cc7f167e2
Emily Palmer <emilymmpalmer@gmail.com>
2024-02-15 11:12:43 -0800

    added revenue section

--- teacher-payments/server.R
+++ teacher-payments/server.R
@@ -2,12 +2,23 @@ library(shiny)
 
 shinyServer(function(input, output) {
     
+    
+    output$revenue_calc <- renderText({
+        paste("(",input$paypal,"+",input$square,"+",input$cash,") - (",
+              input$other_expenses,"+", input$venue,")")
+    })
+    output$revenue <- renderText({
+        revenue <- (input$paypal + input$square + input$cash) - (input$other_expenses + input$venue)
+        paste("Total revenue = ", revenue )
+    })
+    
     output$teacherInputs <- renderUI({
         num_teachers <- input$numTeachers
         teacher_inputs <- lapply(1:num_teachers, function(i) {
             teacher_input <- list(
-                textInput(inputId = paste0("teacher_", i), label = paste("Teacher", i, "Name")),
-                numericInput(inputId = paste0("lessons_", i), label = paste("Number of Lessons", i), value = 1)
+                textInput(inputId = paste0("teacher_", i), label = paste("Teacher", i, "Name"),
+                          value = "name"),
+                numericInput(inputId = paste0("lessons_", i), label = paste("Number of Lessons Taught", i), value = 1)
             )
             div(teacher_input, style = "display: flex; align-items: center;")
         })
@@ -18,7 +29,7 @@ shinyServer(function(input, output) {
         teacher_payments <- sapply(1:input$numTeachers, function(i) {
             input[[paste0("lessons_", i)]]
         })
-        paste("Number of lessons:", sum(teacher_payments))
+        paste("Number of lessons (weeks):", sum(teacher_payments)/2)
     })
  
     
@@ -28,11 +39,11 @@ shinyServer(function(input, output) {
         })
         total_teacher_payment <- sum(teacher_payments)
         
-        total_revenue <- input$totalRevenue
-        
+        total_revenue <- (input$paypal + input$square + input$cash) - (input$other_expenses + input$venue)
         remaining_profit <- max(0,(total_revenue - total_teacher_payment))
         
-        paste("Remaining profit:", remaining_profit, sep = "\n")
+        paste("Remaining profit: Revenue - teacher base rate = ",
+              total_revenue, "-", total_teacher_payment, "=", remaining_profit, sep = "\n")
     })   
        
     output$corvallis_swing_share <- renderText({
@@ -40,16 +51,17 @@ shinyServer(function(input, output) {
             input[[paste0("lessons_", i)]] * input$hourlyRate * input$lessonLength
         })
         total_teacher_payment <- sum(teacher_payments)
-        total_revenue <- input$totalRevenue
+        total_revenue <- (input$paypal + input$square + input$cash) - (input$other_expenses + input$venue)
         remaining_profit <- total_revenue - total_teacher_payment
         if (remaining_profit > 0)  {
-            corvallis_share <- remaining_profit*input$swingFraction
+            corvallis_share <- remaining_profit*0.01*input$swingFraction
         }
         else{
             corvallis_share <- remaining_profit
         }
         
-        paste("Corvallis Swing Dance Society:", corvallis_share)
+        paste("Corvallis Swing Dance Society Share: = Remaining profit * Share = ", 
+              remaining_profit, "*", input$swingFraction, "=", corvallis_share)
     })
     
     output$teacher_share <- renderText({
@@ -57,11 +69,11 @@ shinyServer(function(input, output) {
             input[[paste0("lessons_", i)]] * input$hourlyRate * input$lessonLength
         })
         total_teacher_payment <- sum(teacher_payments)
-        total_revenue <- input$totalRevenue
+        total_revenue <- (input$paypal + input$square + input$cash) - (input$other_expenses + input$venue)
         remaining_profit <- max(0,(total_revenue - total_teacher_payment))
-        teacher_share <- remaining_profit*(1-input$swingFraction)
+        teacher_share <- remaining_profit*(1 - 0.01*input$swingFraction)
         
-        paste("Overall teacher share:", teacher_share)
+        paste("Overall teacher share to be split based on classes taught:", teacher_share)
     })
     
     
@@ -71,10 +83,10 @@ shinyServer(function(input, output) {
             input[[paste0("lessons_", i)]]
         }))
 
-        total_revenue <- input$totalRevenue
+        total_revenue <- (input$paypal + input$square + input$cash) - (input$other_expenses + input$venue)
         lesson_length <- input$lessonLength
         hourly_rate <- input$hourlyRate
-        swing_fraction <- input$swingFraction
+        swing_fraction <- 0.01 * input$swingFraction
         
         wage_cost <- total_lessons * lesson_length * hourly_rate
         remaining_split <- total_revenue - wage_cost 
--- teacher-payments/ui.R
+++ teacher-payments/ui.R
@@ -4,23 +4,37 @@ shinyUI(fluidPage(
     titlePanel("Teacher Payment Calculator"),
     sidebarLayout(
         sidebarPanel(
-            numericInput(inputId = "numTeachers", label = "Number of Teachers", value = 1, min = 1),
-            numericInput(inputId = "totalRevenue", label = "Total Revenue", value = 100),
-            numericInput(inputId = "lessonLength", label = "Lesson Length (hours)", value = 1),
-            numericInput(inputId = "hourlyRate", label = "Hourly Rate", value = 25),
+            numericInput(inputId = "paypal", label = "PayPal Revenue", value = 100),
+            numericInput(inputId = "square", label = "Square Revenue", value = 100),
+            numericInput(inputId = "cash", label = "Cash Revenue", value = 100),
+            numericInput(inputId = "venue", label = "Venue Cost", value = 100),
+            numericInput(inputId = "other_expenses", label = "Other Expenses", value = 0),
+            hr(style = "border-top: 1px solid #000000;"),
+            numericInput(inputId = "numTeachers", label = "Number of Teachers", value = 4, min = 1),
+            numericInput(inputId = "lessonLength", label = "Lesson Length (hours)", value = 1), 
+            numericInput(inputId = "hourlyRate", label = "Hourly Teacher Rate", value = 25),
             numericInput(inputId = "swingFraction", label = "Swing Society Share (%)",
-                         value = .25, min = 0, max = 1)
+                         value = 25, min = 0, max = 100), 
+            submitButton()
         ),
-        
         mainPanel(
+            h2("Revenue Calculation"),
+            p("Revenue Calculation: (PayPal + Square + Cash) - (Venue Cost + Other Expenses) "),
+            textOutput("revenue_calc"),
+            h4(textOutput("revenue")),
+            hr(),
+            h3("Teacher Inputs"),
             uiOutput("teacherInputs"),
-            h4("Teacher payments"),
+            #actionButton("add_teacher", label = "Add Teacher"),
+            h2("Teacher payments"),
             tableOutput("payments"), 
-            h4("Calculations"), 
+            hr(),
+            h2("Calculations"), 
             textOutput("total_lessons"),
             textOutput("profit_to_split"),
             textOutput("corvallis_swing_share"),
             textOutput("teacher_share")
         )
     )
+
 ))
